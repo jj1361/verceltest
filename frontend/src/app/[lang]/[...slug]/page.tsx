4c109438a8927669b2be6623aceb67adb9be1120
Joel Johnson <joeljohnson@Joels-MacBook-Air.local>
2024-02-23 22:35:58 -0600

    slug

--- frontend/src/app/[lang]/[...slug]/page.tsx
+++ frontend/src/app/[lang]/[...slug]/page.tsx
@@ -29,5 +29,5 @@ export default async function PageRoute({params}: Props) {
     const page = await getPageBySlug(params.slug, params.lang);
     if (page.data.length === 0) return null;
     const contentSections = page.data[0].attributes.contentSections;
-    return contentSections.map((section: any, index: number) => sectionRenderer(section, index));
+    return contentSections?.map((section: any, index: number) => sectionRenderer(section, index));
 }
